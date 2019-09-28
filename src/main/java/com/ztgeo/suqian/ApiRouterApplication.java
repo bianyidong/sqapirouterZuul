07222f2846f81ad6f36fdcd3104dab45a0cce8d2
bianyidong <1056460744@qq.com>
2019-09-28 15:32:55 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	pom.xml
    #	src/main/resources/libs/bcprov-jdk14-1.60.jar
    #	src/main/resources/libs/fastjson-1.2.31.jar
    #	src/main/resources/libs/jce.jar
    #	src/main/resources/libs/json-lib-2.3-jdk13.jar
    #	src/main/resources/libs/signTools.jar
    #	src/main/resources/libs/sign_v2_java14.jar
    #	src/main/resources/libs/sunjce_provider.jar

--- src/main/java/com/ztgeo/suqian/ApiRouterApplication.java
+++ src/main/java/com/ztgeo/suqian/ApiRouterApplication.java
@@ -36,30 +36,4 @@ public class ApiRouterApplication {
 //	public ResponseFilter responseFilter(){
 //		return new ResponseFilter();
 //	}
-@Bean
-public Connector connector(){
-	Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-	connector.setScheme("http");
-	connector.setPort(8001);
-	connector.setSecure(false);
-	connector.setRedirectPort(443);
-	return connector;
-}
-
-	@Bean
-	public TomcatServletWebServerFactory tomcatServletWebServerFactory(){
-		TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-		tomcat.addAdditionalTomcatConnectors(connector());
-		return tomcat;
-	}
 }
